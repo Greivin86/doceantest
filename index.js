6cbdde53647a57a75ce949e30a1ae707483a4cda
Greivin86 <109868663+Greivin86@users.noreply.github.com>
2022-08-14 16:47:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const app = express()
-
+const port = process.env.PORT || 3000
 const home = (req, res) => {
 	res.render('home')
 }
 
 app.get('/', home)
 
-app.listen(3000)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
